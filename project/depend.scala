139ce6fe6ca8a48d38be7943019ce1b9439a6470
Nick Hibberd <nhibberd@gmail.com>
2014-11-25 00:40:56 +1100

    bumping saws

--- project/depend.scala
+++ project/depend.scala
@@ -13,7 +13,7 @@ object depend {
                       "org.specs2"           %% "specs2-matcher-extra",
                       "org.specs2"           %% "specs2-scalacheck").map(_ % "2.4.5" % "test")
 
-  val sawsVersion = "1.2.1-20141124065455-da56e5e"
+  val sawsVersion = "1.2.1-20141124133643-c5df52d"
   val saws      = Seq("com.ambiata"          %% "saws-s3"            % sawsVersion excludeAll(
     ExclusionRule(organization = "org.specs2"),
     ExclusionRule(organization = "javax.mail"),
