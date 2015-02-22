1f4141bdb32e1b6681ca83645fa46f7db1f862c7
Nick Hibberd <nhibberd@gmail.com>
2015-02-23 10:41:26 +1100

    Changing DistCopy to use thrift

--- notion-distcopy/src/main/scala/com/ambiata/notion/distcopy/Mappings.scala
+++ notion-distcopy/src/main/scala/com/ambiata/notion/distcopy/Mappings.scala
@@ -1,10 +1,31 @@
 package com.ambiata.notion.distcopy
 
-import argonaut._, Argonaut._
+import com.ambiata.saws.s3._
 
-case class Mappings(mappings: Vector[Mapping])
+import org.apache.hadoop.fs.Path
+
+import scala.collection.JavaConverters._
+
+case class Mappings(mappings: Vector[Mapping]) {
+  def toThrift: MappingsLookup =
+    new MappingsLookup(mappings.toList.map({
+      case UploadMapping(f, t) =>
+        MappingLookup.upload(new UploadMappingLookup(f.toString, t.bucket, t.key))
+      case DownloadMapping(f, t) =>
+        MappingLookup.download(new DownloadMappingLookup(f.bucket, f.key, t.toString))
+    }).asJava)
+
+}
 
 object Mappings {
-  implicit def MappingsCodecJson: CodecJson[Mappings] =
-    casecodec1(Mappings.apply, Mappings.unapply)("dist-copy-mappings")
+  def fromThrift(m: MappingsLookup): Mappings =
+    Mappings(m.mappings.asScala.toVector.map(m =>
+      if (m.isSetUpload) {
+        val up = m.getUpload
+        UploadMapping(new Path(up.path), S3Address(up.bucket, up.key))
+      } else {
+        val d = m.getDownload
+        DownloadMapping(S3Address(d.bucket, d.key), new Path(d.path))
+      }
+    ))
 }
--- notion-distcopy/src/main/scala/com/ambiata/notion/distcopy/Workload.scala
+++ notion-distcopy/src/main/scala/com/ambiata/notion/distcopy/Workload.scala
@@ -1,11 +1,3 @@
 package com.ambiata.notion.distcopy
 
-import argonaut._, Argonaut._
-
-
 case class Workload(indexes: Vector[Int])
-
-object Workload {
-  implicit def WorkloadCodecJson: CodecJson[Workload] =
-    casecodec1(Workload.apply, Workload.unapply)("index")
-}
