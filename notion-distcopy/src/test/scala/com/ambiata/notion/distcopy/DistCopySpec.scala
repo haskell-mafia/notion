2e834a56016c4cd07c3073e19b9602dbc483dfe4
Eric Torreborre <etorreborre@yahoo.com>
2015-05-06 11:15:11 +1000

    properly glob files when uploading a directory to S3

--- notion-distcopy/src/test/scala/com/ambiata/notion/distcopy/DistCopySpec.scala
+++ notion-distcopy/src/test/scala/com/ambiata/notion/distcopy/DistCopySpec.scala
@@ -12,6 +12,7 @@ import org.apache.hadoop.fs.Path
 import org.scalacheck._
 import com.ambiata.mundane.testing.RIOMatcher._
 import com.ambiata.saws.testing._
+import Arbitraries._
 
 class DistCopySpec extends AwsScalaCheckSpec(tests = 5) { def is = s2"""
 
@@ -23,6 +24,11 @@ class DistCopySpec extends AwsScalaCheckSpec(tests = 5) { def is = s2"""
     if no file exists $uploadMappingIsSome
     if a file exists  $uploadMappingIsNone
 
+  A download mapping must contain
+   the full path of the file to copy $createDownloadMappingForADirectory
+
+  An upload mapping must contain
+   the full path of the file to copy $createUploadMappingForADirectory
 """
 
   def downloadMappingIsSome = prop { (s3Temp: S3Temporary, hdfsTemp: HdfsTemporary) =>
@@ -37,7 +43,7 @@ class DistCopySpec extends AwsScalaCheckSpec(tests = 5) { def is = s2"""
     for {
       path    <- hdfsTemp.path.run(configuration)
       address <- s3Temp.address.execute(s3Client)
-      _       <- Hdfs.write(new Path(path, FilePath.unsafe(address.key).basename.name), "old lines").run(configuration)
+      _       <- Hdfs.write(new Path(path, FilePath.unsafe(address.key).path), "old lines").run(configuration)
       result  <- DistCopy.createDownloadMapping(HdfsLocation(path.toString), locationIO)(address)
     } yield result must beNone
   }
@@ -54,11 +60,33 @@ class DistCopySpec extends AwsScalaCheckSpec(tests = 5) { def is = s2"""
     for {
       path    <- hdfsTemp.path.run(configuration)
       prefix  <- s3Temp.prefix.execute(s3Client)
-      address =  prefix | FilePath.unsafe(path.toString).basename.name
+      address =  S3Address(prefix.bucket, prefix.prefix + path.toUri.getPath)
       _       <- address.put("old lines").execute(s3Client)
       result  <- DistCopy.createUploadMapping(S3Location(prefix.bucket, prefix.prefix), locationIO)(path)
     } yield result must beNone
   }
+
+  def createDownloadMappingForADirectory = prop { (s3Temp: S3Temporary, hdfsTemp: HdfsTemporary, filePath: FilePath) =>
+    for {
+      dir      <- hdfsTemp.path.run(configuration)
+      prefix   <- s3Temp.prefix.execute(s3Client)
+      address  =  prefix | filePath.path
+      result   <- DistCopy.createDownloadMapping(HdfsLocation(dir.toString), locationIO)(address)
+    } yield result must beSome(DownloadMapping(address, new Path(dir, address.key)))
+  }
+
+  def createUploadMappingForADirectory = prop { (s3Temp: S3Temporary, hdfsTemp: HdfsTemporary, filePath: FilePath) =>
+    for {
+      dir     <- hdfsTemp.path.run(configuration)
+      path    =  new Path(dir, filePath.path)
+      address <- s3Temp.pattern.execute(s3Client)
+      result  <- DistCopy.createUploadMapping(S3Location(address.bucket, address.unknown), locationIO)(path)
+    } yield result must beSome(UploadMapping(path, S3Address(address.bucket, address.unknown + path.toUri.getPath)))
+  }
+
+  /**
+   * HELPERS
+   */
 
   def locationIO = LocationIO(new Configuration, Clients.s3)
   def configuration = locationIO.configuration
